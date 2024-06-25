bb838dd25a48899d6856ae5fefc3f5d00f4c42ea
Julian-Panditharatne <julianpanditharatne@hotmail.com>
2024-06-25 17:12:36 -0400

    Made fourth, sixth, and seventh lists, might have to remake third list

--- EmployeeSQL/Data Analysis.sql
+++ EmployeeSQL/Data Analysis.sql
@@ -21,15 +21,36 @@ SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
 	ORDER BY dm.dept_no ASC;
 
 -- List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
-
+SELECT de.dept_no, de.emp_no, e.last_name, e.first_name, d.dept_name
+	FROM employees as e
+	INNER JOIN dept_emp as de ON
+	e.emp_no = de.emp_no
+	INNER JOIN departments as d ON
+	de.dept_no = d.dept_no
+	ORDER BY e.emp_no ASC;
 
 -- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
 
 
 -- List each employee in the Sales department, including their employee number, last name, and first name.
-
+SELECT de.emp_no, e.last_name, e.first_name
+	FROM employees as e
+	INNER JOIN dept_emp as de ON
+	e.emp_no = de.emp_no
+	INNER JOIN departments as d ON
+	de.dept_no = d.dept_no
+	WHERE d.dept_name = 'Sales'
+	ORDER BY de.emp_no ASC;
 
 -- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
-
+SELECT de.emp_no, e.last_name, e.first_name, d.dept_name
+	FROM employees as e
+	INNER JOIN dept_emp as de ON
+	e.emp_no = de.emp_no
+	INNER JOIN departments as d ON
+	de.dept_no = d.dept_no
+	WHERE d.dept_name = 'Sales'
+	OR d.dept_name = 'Development'
+	ORDER BY de.emp_no ASC;
 
 -- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
